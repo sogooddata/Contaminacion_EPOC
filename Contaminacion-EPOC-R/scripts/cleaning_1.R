885f4a225f504ed3b5c7e8af845b76459b282b66
rdemedrano <rdemedrano@dia.uned.es>
2020-04-13 15:08:45 +0200

    Cambios menores en limpieza de código y datos de la regresión.

--- Contaminacion-EPOC-R/scripts/cleaning_1.R
+++ Contaminacion-EPOC-R/scripts/cleaning_1.R
@@ -14,7 +14,14 @@ epoc_data[, c("Fecha de nacimiento", "Fecha de ingreso", "Fecha de alta", "Fecha
 # Solo se trabaja con datos de la Comunidad de Madrid, por lo que ciertas columnas no son necesarias. Además
 # la columna Fecha coincide con el año de la Fecha de alta.
 epoc_data[, c("Año" ,"Provincia", "Comunidad Autónoma") := NULL]
-epoc_data <- epoc_data[grepl("491|492|496",`Diagnóstico Principal`)]
+# epoc_data <- epoc_data[grepl("491|492|496",`Diagnóstico Principal`)]
+# epoc_data <- epoc_data[grepl("491|492|493|495|496",paste(`Diagnóstico Principal`, `Diagnóstico 2`, `Diagnóstico 3`,
+#                                              `Diagnóstico 4`, `Diagnóstico 5`, `Diagnóstico 6`, `Diagnóstico 7`,
+#                                              `Diagnóstico 8`, `Diagnóstico 9`, `Diagnóstico 10`))]
 
-save(epoc_data, file = "cleaned_data/epoc_data.RData", compress = "xz")
+# save(epoc_data, file = "cleaned_data/epoc_data.RData", compress = "xz")
 
+# Count es epoc_data[year(Fecha de ingreso) == c(2013,2014,2015),.N, by = list(Hospital)] después de ordenarlo de mayor a menor.
+# par(mfrow=c(2,1))
+# barplot(height = casos_totales_13a15$N, names = casos_totales_13a15$Hospital, las = 2,  cex.names = 0.75)
+# barplot(height = count$N, names =count$`Hospital Recodificado`, las = 2, cex.names = 0.75)
